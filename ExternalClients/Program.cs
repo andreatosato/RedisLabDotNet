7187e44248b27db14d893bb8a1c4f509bd9644d8
Andrea Tosato <andrea.tosato@hotmail.it>
2021-11-15 21:03:45 +0000

    Add more sample

---
+++ ExternalClients/Program.cs
@@ -0,0 +1,38 @@
+﻿using StackExchange.Redis;
+using System.Security.Cryptography;
+
+IConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+        new ConfigurationOptions
+        {
+            EndPoints = { "20.203.217.149:6379" }
+        });
+
+Console.WriteLine("Connected");
+var db = redis.GetDatabase();
+var key = "MultiClientParallel";
+var fieldMessage = "Message";
+var fieldRandomTemperature = "Temperature";
+
+Console.WriteLine("Quanti clients?");
+int clientsNumber = int.Parse(Console.ReadLine()!);
+
+Parallel.For(0, clientsNumber, new ParallelOptions { MaxDegreeOfParallelism = clientsNumber }, async (i) => await SimulateClient(i));
+
+async Task SimulateClient(int number)
+{
+    var dbThreadConnection = redis.GetDatabase(asyncState: number);
+    int i = 0;
+    while (true)
+    {
+        await dbThreadConnection.StreamAddAsync(key, new NameValueEntry[] {
+            new NameValueEntry(fieldMessage, $"{i} - Thread: {Thread.CurrentThread.ManagedThreadId}"),
+            new NameValueEntry(fieldRandomTemperature, RandomNumberGenerator.GetInt32(-10, 40))
+        });
+        Console.WriteLine($"Add {i} - Thread: {Thread.CurrentThread.ManagedThreadId}");
+        i++;
+        await Task.Delay(RandomNumberGenerator.GetInt32(0, 1000));
+    }
+}
+
+Console.WriteLine(".....Startup Completato....");
+Console.Read();
---
+++ src/SimpleClientsParallel/Program.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+using System.Security.Cryptography;
+
+IConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+        new ConfigurationOptions
+        {
+            EndPoints = { "localhost:6379" }
+        });
+var db = redis.GetDatabase();
+var key = "MultiClientParallel";
+int serversNumber = 200;
+
+Parallel.For(0, serversNumber, new ParallelOptions { MaxDegreeOfParallelism = serversNumber }, async (i) => await SimulateServer(i));
+
+async Task SimulateServer(int number)
+{
+    RedisValue position = 0;
+    while (true)
+    {
+        var entries = await db.StreamReadAsync(key, StreamPosition.Beginning);
+        for (int i = 0; i < entries.Length; i++)
+        {
+            foreach (var v in entries[i].Values)
+            {
+                Console.WriteLine($"Read [{entries[i].Id}]: {v.Name} - {v.Value}");
+            }
+        }
+        var idS = entries.Select(t => t.Id);
+        if (idS.Any())
+        {
+            position = idS.OrderBy(t => t).FirstOrDefault();
+            await db.StreamDeleteAsync(key, entries.Select(t => t.Id).ToArray());
+        }
+    }
+}
+
+
+Console.WriteLine(".....Startup Completato....");
+Console.Read();
